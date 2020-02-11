715ccb5bf20a3bbef474136747593e0b416bd814
John Maheswaran <john.maheswaran@yale.fake>
2020-02-11 12:54:47 -0500

    fixing method name conflict

--- dataapi.py
+++ dataapi.py
@@ -9,7 +9,7 @@ def get_db():
 	db = getattr(g, '_database', None)
 	if db is None:
 		db = g._database = sqlite3.connect(DATABASE)
-		db.execute('CREATE TABLE IF NOT EXISTS students (name TEXT, sex TEXT, age INT)')
+		db.execute('CREATE TABLE IF NOT EXISTS users (name TEXT, sex TEXT, age INT)')
 
 		db.row_factory = sqlite3.Row
 	return db
@@ -32,17 +32,17 @@ def init_db():
 			db.cursor().executescript(f.read())
 		db.commit()
 
-def add_student(name='test', age=10, sex='male'):
-	# students is the name of the table in the sqldb
-	sql = "INSERT INTO students (name, sex, age) VALUES('%s', '%s', %d)" %(name, sex, int(age))
+def add_user_to_db(name='test', age=10, sex='male'):
+	# users is the name of the table in the sqldb
+	sql = "INSERT INTO users (name, sex, age) VALUES('%s', '%s', %d)" %(name, sex, int(age))
 	print(sql)
 	db = get_db()
 	db.execute(sql)
 	res = db.commit()
 	return res
 
-def find_student(name=''):
-	sql = "select * from students where name = '%s' limit 1" %(name)
+def find_user_from_db(name=''):
+	sql = "select * from users where name = '%s' limit 1" %(name)
 	print(sql)
 	db = get_db()
 	rv = db.execute(sql)
@@ -57,13 +57,13 @@ def users():
 
 @app.route('/add',methods=['POST'])
 def add_user():
-	print(add_student(name=request.form['name'], age=request.form['age'], sex=request.form['sex']))
+	print(add_user_to_db(name=request.form['name'], age=request.form['age'], sex=request.form['sex']))
 	return ''
 
 @app.route('/add',methods=['GET'])
 def find_user_by_name():
 	name = request.args.get('name', '')
-	student = find_student(name)
-	return jsonify(name=student['name'], age=student['age'], sex=student['sex'])
+	user = find_user_from_db(name)
+	return jsonify(name=user['name'], age=user['age'], sex=user['sex'])
 
 if __name__ == '__main__' : app.run(debug=True)
